367f196bf73eaf8b99b0b2c6c98d595cf447ffcc
David Rabkin <david@rabkin.co.il>
2020-08-20 23:08:04 +0300

    Adds bak.sh

---
+++ app/bak.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -e
+# vi:ts=2 sw=2 tw=79 et lbr wrap
+# Copyright 2016-current David Rabkin
+# bak.sh <arc|box>
+
+IAM=$(basename -- "$0")
+NME="${IAM%.*}"
+LOG="/tmp/$NME-log"
+LCK="/tmp/$NME-lck"
+SRC="/home/david/nas-$1/"
+DST="/media/usb-bak/bak-$1"
+
+log() {
+  date +"%Y%m%d-%H:%M:%S $*" | tee -a "$LOG"
+}
+
+if [ ! -d "$SRC" ]; then
+  log "There is no source directory $SRC."
+  exit 1
+fi
+
+if [ ! -d "$DST" ]; then
+  log "There is no destination directory $DST."
+  exit 1
+fi
+
+# Prevents multiple instances.
+if [ -e "$LCK" ] && kill -0 "$(cat "$LCK")"; then
+  log "$NME is already running."
+  exit 0
+fi
+
+# Makes sure the lockfile is removed when we exit and then claim it.
+# shellcheck disable=SC2064
+trap "rm -f $LCK" INT TERM EXIT
+echo $$ > "$LCK"
+log "$NME says hi, $SRC->$DST."
+rdiff-backup --print-statistics \
+             --terminal-verbosity 4 \
+             --preserve-numerical-ids \
+             --force \
+             "$SRC" "$DST" \
+             2>&1 | tee -a "$LOG"
+log "$NME says bye, $SRC->$DST."
+exit 0
