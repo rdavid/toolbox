bd1b235459b8f7641cca1b0137016a94d9385b6e
David Rabkin <david@rabkin.co.il>
2020-09-30 16:18:25 +0300

    Move duration function to base

--- app/perm.sh
+++ app/perm.sh
@@ -1,26 +1,17 @@
 #!/bin/sh -e
 # vi:ts=2 sw=2 tw=79 et lbr wrap
 # Copyright 2019-present David Rabkin
-#
 # Sets owner to foobar user, set right permitions for files and directories.
 
-# Calculates duration time for report.
-duration() {
-  dur="$(($(date +%s) - $1))"
-  printf "%d:%02d:%02d" \
-    $((dur / 3600)) \
-    $((dur % 3600 / 60)) \
-    $((dur % 60))
-}
+# shellcheck source=./base
+. "$(dirname "$0")/base"
 
 if [ 0 -eq $# ]; then
-  printf 'perm.sh <directory name>\n'
-  exit 0
+  die 'perm.sh <directory name>'
 fi
 DIR=$1
 if [ ! -d "$DIR" ]; then
-  printf 'Directory %s does not exist.\n' "$DIR"
-  exit 0
+  die "Directory $DIR does not exist."
 fi
 printf 'Run %s, are you sure? [y/N] ' "$DIR"
 CFG=$(stty -g)
@@ -29,8 +20,6 @@ printf '\n'
 if ! echo "$ans" | grep -iq "^y"; then
   exit 0
 fi
-BEG="$(date +%s)"
 chown -R foobar "$DIR"
 find "$DIR" -type d -exec chmod 755 {} \;
 find "$DIR" -type f -exec chmod 644 {} \;
-printf 'Done in %s seconds.\n' "$(duration "$BEG")"
