781a1981ebe3125c5e59b4c10991f5314a831bb1
David Rabkin <david@rabkin.co.il>
2020-09-30 18:20:14 +0300

    bak: provide src and dst via parameters

--- app/bak.sh
+++ app/bak.sh
@@ -1,21 +1,23 @@
 #!/bin/sh -e
 # vi:ts=2 sw=2 tw=79 et lbr wrap
 # Copyright 2016-current David Rabkin
-# bak.sh <arc|box>
+# Makes incremental backups from the first parameter to the second.
 
 # shellcheck source=./base
 . "$(dirname "$0")/base"
-SRC="/home/david/nas-$1/"
-DST="/media/usb-bak/bak-$1"
 
-be_root
 validate 'rdiff-backup'
-[ -d "$SRC" ] || die "There is no source directory $SRC."
-[ -d "$DST" ] || die "There is no destination directory $DST."
+
+# Checks amount of parameters.
+if [ "$#" -ne 2 ]; then
+  die "Usage: $IAM [source] [destination]"
+fi
+[ -r "$1" ] || die "Unable to read from source $1."
+[ -w "$2" ] || die "Unable to write to destination $2."
 rdiff-backup --print-statistics \
              --terminal-verbosity 4 \
              --preserve-numerical-ids \
              --force \
-             "$SRC" "$DST" \
+             "$1" "$2" \
              2>&1 | tee -a "$LOG"
 exit 0
