00cd4ae2d8aa8ef1fe727e0307202345f6680a77
David Rabkin <david@rabkin.co.il>
2020-11-23 16:25:21 +0200

    tru: add authentication option

--- app/tru.sh
+++ app/tru.sh
@@ -7,41 +7,53 @@
 
 # shellcheck source=./base
 . "$(dirname "$(realpath "$0")")/base"
+AUT=''
 CMD='transmission-remote'
 
 # Looks for torrent ID by torrent file name.
 tid() {
-  $CMD "$SER" -l | grep "$FIL" | awk '{print $1}'
+  # SC2086: Double quote to prevent globbing and word splitting.
+  # shellcheck disable=SC2086
+  $CMD "$SER" $AUT -l | grep "$FIL" | awk '{print $1}'
 }
 
 validate 'nc'
 validate 'tr'
 validate 'awk'
 validate $CMD
-[ "$#" -eq 2 ] || die "Usage: $BASE_IAM [host:port] [filename]"
+[ "$#" -eq 3 ] || die "Usage: $BASE_IAM HOST:PORT [USR]:[PWD] TORRENT-FILE"
 
 # Prepares host and port to be nc command parameters. Validates the first
 # parameter host:port.
 prm="$(printf '%s' "$1" | tr ':' ' ')"
 # shellcheck disable=SC2086
-nc -z $prm >/dev/null 2>&1 || die "$1 is not valid parameter for [host:port]."
+nc -z $prm >/dev/null 2>&1 || die "$1 is not valid parameter for HOST:PORT."
 SER="$1"
 
-# Validates second parameter full path name.
-[ -r "$2" ] || die "$2 is not valid parameter for a torrent file."
-TOR="$2"
+# Single ':' means there is no authentication data.
+if [ "$2" != ':' ]; then
+  AUT="--auth $2"
+fi
+
+# Validates third parameter full torrent file name.
+[ -r "$3" ] || die "$3 is not valid parameter for a TORRENT-FILE."
+TOR="$3"
 FIL=$(basename -- "$TOR")
 FIL="${FIL%.*}"
 
 # Looks for torrent ID by torrent name extracted from torrent file name.
 tid=$(tid)
 if [ -n "$tid" ]; then
-  $CMD "$SER" -t "$tid" --remove-and-delete 2>&1 | tee -a "$BASE_LOG"
+  # SC2086: Double quote to prevent globbing and word splitting.
+  # shellcheck disable=SC2086
+  $CMD "$SER" $AUT -t "$tid" --remove-and-delete 2>&1 | tee -a "$BASE_LOG"
   log "$FIL $tid is removed from $SER."
 fi
 
 # Adds torrent file name to torrent server.
-$CMD "$SER" -a "$TOR" 2>&1 | tee -a "$BASE_LOG"
+# SC2086: Double quote to prevent globbing and word splitting.
+# shellcheck disable=SC2086
+$CMD "$SER" $AUT -a "$TOR" 2>&1 | tee -a "$BASE_LOG"
 
 # Verifies that a torrent was added.
 tid=$(tid)
