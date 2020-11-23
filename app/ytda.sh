28c9618ac9d445ba46bba29ee0c9958138dec7ab
David Rabkin <david@rabkin.co.il>
2020-11-23 15:28:48 +0200

    ytda: use cookies.txt, keep files in case of rsync failure

--- app/ytda.sh
+++ app/ytda.sh
@@ -7,10 +7,11 @@
 
 # shellcheck source=./base
 . "$(dirname "$(realpath "$0")")/base"
-TMP="/$BASE_LCK/out"
+OUT="/$BASE_LCK/out"
 DST='/mnt/nas-ibx/ytb'
 ARC='/mnt/nas-ibx/ytb/app/done.txt'
 SRC='/mnt/nas-ibx/ytb/app/channels.txt'
+CKS='/mnt/nas-ibx/ytb/app/cookies.txt'
 
 # The script is ran by cron, the environment is stricked.
 export LC_ALL=en_US.UTF-8
@@ -22,12 +23,22 @@ validate 'youtube-dl'
 [ -r $SRC ] || die "Unable to read $SRC."
 [ -w $DST ] || die "Unable to write $DST."
 [ -w $ARC ] || die "Unable to write $ARC."
-mkdir -p "$TMP" || die "Unable to create $TMP."
+mkdir -p "$OUT" || die "Unable to create $OUT."
+if [ -r $CKS ]; then
+  log "Uses cookie $CKS."
+  CKS_PARAM="--cookies $CKS"
+else
+  CKS_PARAM=''
+fi
+
+# SC2086: Double quote to prevent globbing and word splitting.
+# shellcheck disable=SC2086
 youtube-dl \
+  $CKS_PARAM \
   --playlist-reverse \
   --download-archive $ARC \
   -i -o \
-  "$TMP/%(uploader)s-%(upload_date)s-%(title)s.%(ext)s" \
+  "$OUT/%(uploader)s-%(upload_date)s-%(title)s.%(ext)s" \
   -f bestvideo[ext=mp4]+bestaudio[ext=m4a] \
   --merge-output-format mp4 \
   --add-metadata \
@@ -36,9 +47,33 @@ youtube-dl \
 
 # Do nothing if the output directory is empty.
 # shellcheck disable=SC2010
-if ls -1A "$TMP" | grep -q .; then
-  renamr -d "$TMP" -a 2>&1 | tee -a "$BASE_LOG"
-  rsync -zvhr --progress "$TMP"/* $DST 2>&1 | tee -a "$BASE_LOG"
+if ls -1A "$OUT" | grep -q .; then
+  renamr -d "$OUT" -a 2>&1 | tee -a "$BASE_LOG"
+  rsync -zvhr --progress "$OUT"/* $DST 2>&1 | tee -a "$BASE_LOG"
+
+  # Makes sure every file was copied succesfully to destination. Pass each file
+  # locally and verifies there is a file with a same name at destination.
+  nsrc=0
+  ndst=0
+  pipe="$BASE_LCK/pipe"
+  mkfifo "$pipe"
+  find "$OUT" -type f -exec basename {} \; > "$pipe" &
+  while read -r name; do
+    nsrc=$((nsrc + 1))
+    [ -f "$DST/$name" ] && ndst=$((ndst + 1))
+  done < "$pipe"
+  rm "$pipe"
+
+  # Keeps files locally in case of an rsync failure.
+  if [ $nsrc -ne $ndst ]; then
+    arc="$BASE_TMP/$BASE_IAM-arc"
+    mkdir -p "$arc"
+    mv "$OUT"/* "$arc"
+    loge "There are $nsrc files in $OUT, but $ndst in $DST, archived at $arc."
+  else
+    rm -f "$OUT"/*
+    log "There are $nsrc files copied to $DST."
+  fi
 fi
-rm -rf "$TMP"
+rmdir "$OUT"
 exit 0
