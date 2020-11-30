905cab8d464d2cb5298c0425fe765444a0db3e0e
David Rabkin <david@rabkin.co.il>
2020-11-30 17:32:10 +0200

    Add tex utility to produce pdf from tex file

---
+++ app/tex.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -eu
+# vi:ts=2 sw=2 tw=79 et lbr wrap
+# Copyright 2020-present David Rabkin
+
+# shellcheck source=./base
+. "$(dirname "$(realpath "$0")")/base"
+[ "$#" -eq 1 ] || die "Usage: $BASE_IAM FILENAME.tex"
+[ -r "$1" ] || die "Unable to read from source $1."
+validate 'tex'
+validate 'dvipdfm'
+NME=$(basename -- "$1")
+
+# Makes sure that source filename has tex extention.
+[ 'tex' = "${NME##*.}" ] || die "Source file $1 should have .tex extention."
+
+# Extracts file name only.
+NME="${NME%.*}"
+TEX="$NME.tex"
+DVI="$NME.dvi"
+PDF="$NME.pdf"
+LOG="$NME.log"
+
+# Do not worry about file cleaning on exit. Garbage collector will remove the
+# temporarily directory.
+cp "$1" "$BASE_LCK"
+cd "$BASE_LCK"
+if ! tex --interaction=batchmode "./$TEX" >/dev/null 2>&1; then
+  cp "./$LOG" "$OLDPWD"
+  die "See $LOG for errors."
+fi
+dvipdfm "./$DVI" >/dev/null 2>&1
+cp "./$PDF" "$OLDPWD"
+open "$OLDPWD/$PDF"
+exit 0
