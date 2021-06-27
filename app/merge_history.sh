4251f3c5dc01fe90cb8e6dafce522d5e7331ce32
David Rabkin <david@rabkin.co.il>
2021-06-27 14:11:45 +0300

    Move merge_history.sh to dotfiles

--- app/merge_history.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -eu
-# vi:ts=2 sw=2 tw=79 et lbr wrap
-# Copyright 2018 by David Rabkin
-# Magic, do not touch, see:
-#   https://david-kerwick.github.io/2017-01-04-combining-zsh-history-files/
-cat "$@" | awk -v date="WILL_NOT_APPEAR$(date +"%s")" '{if (sub(/\\$/,date)) printf "%s", $0; else print $0}' | LC_ALL=C sort -u | awk -v date="WILL_NOT_APPEAR$(date +"%s")" '{gsub('date',"\\\n"); print $0}'
