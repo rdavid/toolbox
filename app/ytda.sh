d2301d12300848c7803f087fa1aa9f894be7ae17
David Rabkin <david@rabkin.co.il>
2021-10-31 22:35:49 +0200

    Transcode downloded files to m4v

--- app/ytda.sh
+++ app/ytda.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -eu
 # vi:ts=2 sw=2 tw=79 et lbr wrap
-# Copyright 2019 by David Rabkin
+# Copyright 2019 - 2021 by David Rabkin
 # The script downloads all new video from pre-configured acoounts in
 # channels.txt. It updates IDs of downloaded files at done.txt. The script
-# could be ran by a cron job. Uses yt-dlp, rsync, renamr.
+# could be ran by a cron job. Uses renamr, rsync, transcode, yt-dlp.
 
 # shellcheck source=../../shellbase/inc/base
 . "$(dirname "$(realpath "$0")")/../shellbase/inc/base"
 OUT="/$BASE_LCK/out"
+M4V="$OUT/m4v"
 DST='/mnt/nas-ibx/ytb'
 ARC='/mnt/nas-ibx/ytb/app/done.txt'
 SRC='/mnt/nas-ibx/ytb/app/channels.txt'
@@ -17,13 +18,16 @@ CKS='/mnt/nas-ibx/ytb/app/cookies.txt'
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
+validate 'HandBrakeCLI'
 validate 'rsync'
 validate 'renamr'
+validate 'transcode'
 validate 'yt-dlp'
 [ -r $SRC ] || die "Unable to read $SRC."
 [ -w $DST ] || die "Unable to write $DST."
 [ -w $ARC ] || die "Unable to write $ARC."
 mkdir -p "$OUT" || die "Unable to create $OUT."
+mkdir -p "$M4V" || die "Unable to create $M4V."
 if [ -r $CKS ]; then
   log "Uses cookie $CKS."
   CKS_PARAM="--cookies $CKS"
@@ -49,7 +53,8 @@ yt-dlp \
 # shellcheck disable=SC2010
 if ls -1A "$OUT" | grep -q .; then
   renamr -d "$OUT" -a 2>&1 | tee -a "$BASE_LOG"
-  rsync -zvhr --progress "$OUT"/* $DST 2>&1 | tee -a "$BASE_LOG"
+  transcode -d "$OUT" -o "$M4V" -a 2>&1 | tee -a "$BASE_LOG"
+  rsync -zvhr --progress "$M4V"/* $DST 2>&1 | tee -a "$BASE_LOG"
 
   # Makes sure every file was copied succesfully to destination. Pass each file
   # locally and verifies there is a file with a same name at destination.
@@ -57,20 +62,22 @@ if ls -1A "$OUT" | grep -q .; then
   ndst=0
   pipe="$BASE_LCK/pipe"
   mkfifo "$pipe"
-  find "$OUT" -type f -exec basename {} \; > "$pipe" &
+  find "$M4V" -type f -exec basename {} \; > "$pipe" &
   while read -r name; do
     nsrc=$((nsrc + 1))
     [ -f "$DST/$name" ] && ndst=$((ndst + 1))
   done < "$pipe"
   rm "$pipe"
 
-  # Keeps files locally in case of an rsync failure.
-  if [ $nsrc -ne $ndst ]; then
+  # Keeps files locally in case of a failure.
+  if [ $nsrc -eq 0 ] || [ $nsrc -ne $ndst ]; then
     arc="$BASE_TMP/$BASE_IAM-arc"
     mkdir -p "$arc"
     mv "$OUT"/* "$arc"
     loge "There are $nsrc files in $OUT, but $ndst in $DST, archived at $arc."
   else
+    rm -f "$M4V"/*
+    rmdir "$M4V"
     rm -f "$OUT"/*
     log "There are $nsrc files copied to $DST."
   fi
